cba1662dfed39e810bc60f2e9b4a4c0ab2eb3409
Stefan Reisner <stefan.reisner@online.de>
2019-12-13 15:05:50 +0100

    updates

--- vm/scripts/test.py
+++ vm/scripts/test.py
@@ -1,29 +1,36 @@
 FREECADPATH = '/usr/lib/freecad/lib' # path to your FreeCAD.so or FreeCAD.dll file
-import sys,bpy
+import sys
 sys.path.append(FREECADPATH)
- 
+import bpy
+
 def import_fcstd(filename):
-   try:
-       import FreeCAD
-   except ValueError:
-       print ('import error\n')
-   else:
-       scene = bpy.Scene.GetCurrent()
-       import Part
-       doc = FreeCAD.open(filename)
-       objects = doc.Objects
-       for ob in objects:
-           if ob.Type[:4] == 'Part':
-               shape = ob.Shape
-               if shape.Faces:
-                   mesh = bpy.Mesh.New()
-                   rawdata = shape.tessellate(1)
-                   for v in rawdata[0]:
-                       mesh.verts.append((v.x,v.y,v.z))
-                   for f in rawdata[1]:
-                       mesh.faces.append.append(f)
-                   scene.objects.new(mesh,ob.Name)
-       bpy.Redraw()
+    try:
+        import FreeCAD
+    except ValueError:
+        print ('import error\n')
+    else:
+        scene = bpy.context.scene
+
+        if scene.world is None:
+            # create a new world
+            new_world = bpy.data.worlds.new("New World")
+            new_world.use_sky_paper = True
+            scene.world = new_world
+            
+        import Part
+        doc = FreeCAD.open(filename)
+        objects = doc.Objects
+        for ob in objects:
+            if ob.Type[:4] == 'Part':
+                shape = ob.Shape
+                if shape.Faces:
+                    mesh = bpy.Mesh.New()
+                    rawdata = shape.tessellate(1)
+                    for v in rawdata[0]:
+                        mesh.verts.append((v.x,v.y,v.z))
+                    for f in rawdata[1]:
+                        mesh.faces.append.append(f)
+                    scene.objects.new(mesh,ob.Name)
 
 def main():
     filename=sys.argv[1]
