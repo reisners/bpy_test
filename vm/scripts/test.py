9d4ffa1d0d3e500f1a9e6c639b2f6a41a48468e3
Stefan Reisner <stefan.reisner@online.de>
2019-12-23 10:03:15 +0100

    added .gitignore

--- vm/scripts/test.py
+++ vm/scripts/test.py
@@ -2,6 +2,7 @@ FREECADPATH = '/usr/lib/freecad/lib' # path to your FreeCAD.so or FreeCAD.dll fi
 import sys
 sys.path.append(FREECADPATH)
 import bpy,bmesh
+import math
 
 def import_fcstd(filename):
     try:
@@ -31,10 +32,42 @@ def import_fcstd(filename):
                     obj = bpy.data.objects.new("my_obj", mesh)
                     bpy.context.collection.objects.link(obj)
                     print ( ob.Name + " -> "+str(len(rawdata[0]))+" vertices, " + str(len(rawdata[1]))+ " faces processed\n" )
+
+import mathutils
+import io_export_paper_model
+
+def rotate_and_render(output_dir):
+    origin = bpy.context.object
+    scene = bpy.context.scene
+
+    # Create a light
+    light_data = bpy.data.lights.new('light', type='POINT')
+    light = bpy.data.objects.new('light', light_data)
+    scene.collection.objects.link(light)
+    light.location = mathutils.Vector((3, -4.2, 5))
+
+    # Create the camera
+    cam_data = bpy.data.cameras.new('camera')
+    cam = bpy.data.objects.new('camera', cam_data)
+    scene.collection.objects.link(cam)
+    scene.camera = cam
+
+    cam.location = mathutils.Vector((6, -3, 5))
+    cam.rotation_euler = mathutils.Euler((0.9, 0.0, 1.1))
+
+    scene.render.engine = 'CYCLES'
+    scene.render.image_settings.file_format = 'PNG'
+    scene.render.filepath = output_dir + '/image.png'
+
+    print("rendering\n")
+    bpy.ops.render.render(write_still = True)
+    print("rendered\n")
+
 def main():
     filename=sys.argv[1]
     import_fcstd(filename)
- 
+    rotate_and_render('/work/output/')
+
 # This lets you import the script without running it
 if __name__=='__main__':
    main()
