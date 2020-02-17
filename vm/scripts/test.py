b7ee7f918de9204935a9db22ca8a5cad00db026c
Stefan Reisner <stefan.reisner@online.de>
2020-02-17 23:19:56 +0100

    updated test.py: trying to assemble all facebinder objects into a parent object (work in progress)

--- vm/scripts/test.py
+++ vm/scripts/test.py
@@ -5,7 +5,7 @@ import bpy,bmesh
 import math
 from addon_utils import check, paths, enable
 
-def import_fcstd(filename):
+def convert_to_paper_model(filename, page_size_preset, split_index):
     try:
         import FreeCAD
     except ValueError:
@@ -19,27 +19,67 @@ def import_fcstd(filename):
             new_world.use_sky_paper = True
             scene.world = new_world
             
+        for o in bpy.context.scene.objects:
+            if o.type == 'MESH':
+                o.select_set(True)
+                print(o)
+            else:
+                o.select_set(False)
+
+        # Call the operator only once
+        bpy.ops.object.delete()
+
         import Part
         doc = FreeCAD.open(filename)
+        
+        parent = bpy.data.objects.new("parent", None)
+
         objects = doc.Objects
         for ob in objects:
             print ("found "+ob.TypeId+" "+ob.Name)
-            if ob.TypeId == 'PartDesign::Body':
+            if ob.TypeId == 'Part::FeaturePython':
+                if ob.Faces:
+                    print ("facebinder has "+str(len(ob.Faces))+" faces")
+                    mesh = bpy.data.meshes.new("mesh_"+ob.Name)
+                    rawdata = ob.Shape.tessellate(0.1)
+                    mesh.from_pydata(rawdata[0], [], rawdata[1])
+                    obj = bpy.data.objects.new("obj_"+ob.Name, mesh)
+                    obj.parent = parent
+                    bpy.context.collection.objects.link(obj)
+                    print ( ob.Name + " -> "+str(len(rawdata[0]))+" vertices, " + str(len(rawdata[1]))+ " faces processed\n" )
+            if False: # ob.TypeId == 'PartDesign::Body':
                 shape = ob.Shape
                 if shape.Faces:
+#                    for faceindex, face in enumerate(shape.Faces):
+#                        color=shape.DiffuseColor
+#                        if len(color) ==1:
+#                            facecolor = color[0]
+#                        else:
+#                            facecolor = color[faceindex]
+#                        print (shape.Label, face.Label, color[faceindex])
                     mesh = bpy.data.meshes.new("my_mesh")
                     rawdata = shape.tessellate(0.1)
                     mesh.from_pydata(rawdata[0], [], rawdata[1])
 
-                    # post-process mesh
-                    for edge in mesh.edges:   
-                        edge.use_seam = True
-                    #bpy.ops.uv.smart_project(mesh)
+                    mesh.edges[split_index].use_seam = True
+                    pdffile='/work/output/paper_model.pdf'
+                    #bpy.ops.uv.smart_project()
 
                     obj = bpy.data.objects.new("my_obj", mesh)
                     bpy.context.collection.objects.link(obj)
                     bpy.context.view_layer.objects.active = obj
                     print ( ob.Name + " -> "+str(len(rawdata[0]))+" vertices, " + str(len(rawdata[1]))+ " faces processed\n" )
+
+        bpy.context.collection.objects.link(parent)
+        bpy.context.view_layer.objects.active = parent
+        #bpy.context.view_layer.update()
+
+        pdffile='/work/output/paper_model.pdf'
+        bpy.ops.export_mesh.paper_model(
+            filepath=pdffile, 
+            page_size_preset=page_size_preset, 
+            scale=100)
+
 
 import mathutils
 import io_export_paper_model
@@ -74,16 +114,9 @@ def rotate_and_render(output_dir):
 def main():
     enable('io_export_paper_model')
     filename=sys.argv[1]
-    import_fcstd(filename)
-    blendfile='/work/output/blender.blend'
-    bpy.ops.wm.save_as_mainfile(filepath=blendfile)
-    print("saved to "+blendfile)
-    pdffile='/work/output/paper_model.pdf'
-    #rotate_and_render('/work/output/')
-    bpy.ops.export_mesh.paper_model(
-        filepath=pdffile, 
-        page_size_preset='A4', 
-        scale=100)
+    page_size_preset=sys.argv[2]
+    split_index=int(sys.argv[3])
+    convert_to_paper_model(filename, page_size_preset, split_index)
 
 # This lets you import the script without running it
 if __name__=='__main__':
