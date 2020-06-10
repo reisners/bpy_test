f64e7338996c89860acfa80977604368da7c2f94
Stefan Reisner <stefan.reisner@springer.com>
2020-06-10 08:38:13 +0200

    updated build-freecad-blender,sh: adding commands to push and tag docker image

--- vm/build-freecad-blender.sh
+++ vm/build-freecad-blender.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker build -f Dockerfile-freecad-blender -t freecad-blender .
+sudo docker build -f Dockerfile-freecad-blender -t reisners/freecad-blender .
+sudo docker push reisners/freecad-blender
+sudo docker tag reisners/freecad-blender freecad-blender
