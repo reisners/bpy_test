60a3e857a8285313b94e5e5077277b47022a0c04
Stefan Reisner <stefan.reisner@springer.com>
2020-12-04 21:02:16 +0100

    updated build-freecad-blender.sh: removing sudo

--- vm/build-freecad-blender.sh
+++ vm/build-freecad-blender.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker build -f Dockerfile-freecad-blender -t reisners/freecad-blender .
-sudo docker push reisners/freecad-blender
-sudo docker tag reisners/freecad-blender freecad-blender
+docker build -f Dockerfile-freecad-blender -t reisners/freecad-blender .
+docker push reisners/freecad-blender
+docker tag reisners/freecad-blender freecad-blender
