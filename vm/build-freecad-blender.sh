aa604c57d7ba99eb0c080d78bcb5f806caf2867b
Stefan Reisner <stefan.reisner@springer.com>
2020-12-18 09:22:57 +0100

    updated build-freecad-blender.sh: tagging image as reisners/freecad-blender:master

--- vm/build-freecad-blender.sh
+++ vm/build-freecad-blender.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -f Dockerfile-freecad-blender -t reisners/freecad-blender .
-docker push reisners/freecad-blender
-docker tag reisners/freecad-blender freecad-blender
+docker build -f Dockerfile-freecad-blender -t reisners/freecad-blender:master .
+docker tag reisners/freecad-blender:master freecad-blender
+docker push reisners/freecad-blender:master
