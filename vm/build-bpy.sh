93cecd362389aa41ce0713f635bd32c4bd0107cf
Stefan Reisner <stefan.reisner@springer.com>
2021-07-31 15:33:56 +0200

    docker images with explicit versioning

--- vm/build-bpy.sh
+++ vm/build-bpy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker build -f Dockerfile-bpy -t reisners/bpy:master .
-docker push reisners/bpy:master
-docker tag reisners/bpy:master bpy
+docker build -f Dockerfile-bpy -t reisners/bpy:v1.0 .
+docker push reisners/bpy:v1.0
