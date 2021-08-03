a55e0e9747eed5d6db1851736e04650956ee6bba
Stefan Reisner <stefan.reisner@springer.com>
2021-08-03 10:13:40 +0200

    deleted unused stuff

--- vm/build-blender-freecad.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile-blender-freecad -t blender-freecad .
--- vm/build-freecad-blender.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile-freecad-blender -t reisners/freecad-blender:master .
-docker tag reisners/freecad-blender:master freecad-blender
-docker push reisners/freecad-blender:master
